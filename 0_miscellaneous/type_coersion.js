3e96b52da7d5da45cd2eed1048541bfe8f0bc02e
tobiasvinther <tobi626i@stud.kea.dk>
2022-08-29 09:55:36 +0200

    first commit

---
+++ 0_miscellaneous/type_coersion.js
@@ -0,0 +1,8 @@
+//type coersion lecture
+const total = 34.32
+const price = 12.32
+
+const newTotal = total + price
+//i js ønsker vi aldrig at bruge + i console.log, men bruge kommaer til at separere variabler
+//vi vil ikke manipulere værdier i console
+console.log(newTotal)
