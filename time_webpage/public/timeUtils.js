347d20763f5046bcdc64a93ca295f4b961b4d502
tobiasvinther <tobi626i@stud.kea.dk>
2022-09-21 20:30:28 +0200

    deleted some code that wasn't used

--- time_webpage/public/timeUtils.js
+++ time_webpage/public/timeUtils.js
@@ -1,11 +1,4 @@
-import { initializeScore, initializePlayers, initializeGame } from "./dartGame.js"
-
 const URL = "http://localhost:8080/"
-
-initializeScore(501)
-initializePlayers("Simon", "Silke")
-
-initializeGame()
 
 function getDate() {
     document.getElementById("date_id").innerText = new Date().toDateString()
