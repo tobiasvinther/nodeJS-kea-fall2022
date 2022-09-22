991fd2cb7ce1d92e7b182c45b1d5790693397690
tobiasvinther <tobi626i@stud.kea.dk>
2022-09-22 12:29:53 +0200

    added a silly blink effect and other minor changes

--- time_webpage/public/timeUtils.js
+++ time_webpage/public/timeUtils.js
@@ -1,7 +1,7 @@
 const URL = "http://localhost:8080/"
 
 function getDate() {
-    document.getElementById("date_id").innerText = new Date().toDateString()
+    document.getElementById("date_id").innerText = "Today is:\n" + new Date().toDateString()
 }
 
 getDate()
